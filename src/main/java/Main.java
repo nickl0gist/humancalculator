392c2c2748ed8fbd1197367e2e22767b88c3d93b
nickl0gist <nicklogistick@mail.ru>
2019-06-16 11:11:23 +0200

    static method asker() created in Main class to avoid wrong values provided.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,16 +3,33 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("What is your height?");
-        double height = sc.nextDouble();
-        System.out.println("What is your weight?");
-        double weight = sc.nextDouble();
+
+        double height = asker(sc, "What is your height in meters?");
+        double weight = asker(sc, "What is your weight in kilograms?");
 
         BMICalculator bmi = new BMICalculator(height, weight);
 
         System.out.println(bmi.calculate());
         System.out.println(bmi.interpret());
         sc.close();
+    }
+
+    private static double asker(Scanner sc, String str){
+        System.out.println(str);
+        while(true){
+           try {
+               double answer = Double.parseDouble(sc.next());
+               if (answer > 0){
+                   return answer;
+               } else {
+                   System.out.println("You gave wrong value! Try again. \n" + str);
+               }
+           } catch (NumberFormatException e){
+               System.out.println("You gave wrong value! Try again. \n" + str);
+           }
+        }
     }
 }
