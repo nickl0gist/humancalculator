40dea4725b1ed507410ca94ee029359f9b905b48
nickl0gist <nicklogistick@mail.ru>
2019-06-16 10:30:57 +0200

    BMICalculator finished. Tests Passed

--- src/main/java/BMICalculator.java
+++ src/main/java/BMICalculator.java
@@ -2,16 +2,43 @@ public class BMICalculator implements HumanCalculator{
 
     public static final double BOTTOM_BORDER = 18.5;
     public static final double NORMAL_BORDER = 25.0;
-    public static final double UPPER_BORDER = 25.0;
+    public static final double UPPER_BORDER = 30.0;
 
+    private double height;
+    private double weight;
+    private double index;
 
-    @Override
-    public String calculate() {
-        return null;
+    public BMICalculator(double height, double weight) {
+        this.height = height;
+        this.weight = weight;
     }
 
     @Override
+    public String calculate() {
+        index = weight / (height * height);
+        return String.format("Your BMI index is %.2f", index);
+    }
+
+
+    /**
+     * @return String of interpreted BMI
+     *
+     * Wartość	            Co oznacza?
+     * BMI < 18,5	        niedowaga
+     * 18,5 ≤ BMI < 25	    waga prawidłowa
+     * 25 ≤ BMI < 30	    nadwaga
+     * BMI >= 30            otyłość
+     */
+    @Override
     public String interpret() {
+        if (index < BMICalculator.BOTTOM_BORDER)
+            return "You are underweight.";
+        if(index >= BMICalculator.BOTTOM_BORDER && index < BMICalculator.NORMAL_BORDER)
+            return "Your BMI is in normal condition.";
+        if(index >= BMICalculator.NORMAL_BORDER && index < BMICalculator.UPPER_BORDER)
+            return "You are overweight!";
+        if(index >= BMICalculator.UPPER_BORDER)
+            return "You are obese!";
         return null;
     }
 }
--- src/test/java/BMICalculatorTest.java
+++ src/test/java/BMICalculatorTest.java
@@ -3,37 +3,42 @@ import org.junit.Test;
 
 public class BMICalculatorTest {
 
-
    /**
     * Wartość	            Co oznacza?
     * BMI < 18,5	        niedowaga
     * 18,5 ≤ BMI < 25	    waga prawidłowa
-    * 25 ≤ BMI < 30	    nadwaga
-    * BMI >= 30              otyłość
+    * 25 ≤ BMI < 30	        nadwaga
+    * BMI >= 30             otyłość
     */
 
    @Test
     public void underWeight (){
        BMICalculator bmi = new BMICalculator(1.80, 55.0);
-       Assert.assertTrue(bmi.calculate() < BMICalculator.BOTTOM_BORDER);
+       Assert.assertTrue(bmi.calculate().equals("Your BMI index is 16,98"));
+       Assert.assertTrue(bmi.interpret().equals("You are underweight."));
    }
 
     @Test
     public void normalWeight (){
         BMICalculator bmi = new BMICalculator(1.80, 70.0);
-        Assert.assertTrue(bmi.calculate() >= BMICalculator.BOTTOM_BORDER && bmi.calculate() < BMICalculator.NORMAL_BORDER);
+        Assert.assertTrue(bmi.calculate().equals("Your BMI index is 21,60"));
+        Assert.assertTrue(bmi.interpret().equals("Your BMI is in normal condition."));
     }
 
     @Test
     public void overWeight (){
         BMICalculator bmi = new BMICalculator(1.80, 90.0);
-        Assert.assertTrue(bmi.calculate() >= BMICalculator.NORMAL_BORDER && bmi.calculate() < BMICalculator.UPPER_BORDER);
+        Assert.assertTrue(bmi.calculate().equals("Your BMI index is 27,78"));
+        Assert.assertTrue(bmi.interpret().equals("You are overweight!"));
     }
 
     @Test
     public void obesityWeight (){
         BMICalculator bmi = new BMICalculator(1.80, 120.0);
-        Assert.assertTrue(bmi.calculate() >= BMICalculator.UPPER_BORDER);
+        System.out.println(bmi.calculate());
+        Assert.assertTrue(bmi.calculate().equals("Your BMI index is 37,04"));
+        Assert.assertTrue(bmi.interpret().equals("You are obese!"));
+
     }
 
 }
