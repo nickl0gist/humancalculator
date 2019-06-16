781ee640e74dc64515157e04358738d34d865411
nickl0gist <nicklogistick@mail.ru>
2019-06-16 10:33:43 +0200

    Main class with scanner created

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your height?");
+        double height = sc.nextDouble();
+        System.out.println("What is your weight?");
+        double weight = sc.nextDouble();
+
+        BMICalculator bmi = new BMICalculator(height, weight);
+
+        System.out.println(bmi.calculate());
+        System.out.println(bmi.interpret());
+    }
 }
